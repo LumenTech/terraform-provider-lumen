565f72d4e4b18b25b6c4759563aa0efce9eaf740
Shibayan Chatterjee <shibayan.chatterjee@lumen.com>
2022-11-11 14:04:07 -0700

    BM Plugin release 0.4.7

--- lumen/client.go
+++ lumen/client.go
@@ -30,7 +30,7 @@ type Client struct {
 
 // Setting up new client
 func NewClient(apiUrl string, authUrl string) (client *Client) {
-	var userAgent = "lumen-terraform-plugin v0.4.1"
+	var userAgent = "lumen-terraform-plugin v0.4.7"
 	return &Client{
 		ApiUrl:    apiUrl,
 		AuthUrl:   authUrl,
