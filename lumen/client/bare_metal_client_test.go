bfe85e64add3b850d01c58bd284f813ea914784c
molsbee <molsbee@gmail.com>
2023-11-01 09:36:28 -0500

    Working on creating a generic test server function that can reduce duplication.

--- lumen/client/bare_metal_client_test.go
+++ lumen/client/bare_metal_client_test.go
@@ -11,29 +11,50 @@ import (
 	"time"
 )
 
-func TestRefreshToken_NotExpired(t *testing.T) {
-	apigeeCallCount := 0
-	issuedAt := time.Now().UnixMilli()
-	expiresIn := int64(1800)
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+type HttpResponses []HttpResponse
+
+type HttpResponse struct {
+	StatusCode int
+	Body       interface{}
+}
+
+func setupTestServer(t *testing.T, apigeeResponses HttpResponses, apigeeCallCount *int) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		if req.RequestURI == "/oauth/token" {
-			apigeeCallCount++
+			*apigeeCallCount++
 			assert.Equal(t, "POST", req.Method)
 			assert.Contains(t, req.Header.Get("Authorization"), "Basic")
 			assert.Equal(t, "application/json", req.Header.Get("Accept"))
 			assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
 			assert.Equal(t, "lumen-terraform-plugin v0.5.3", req.Header.Get("User-Agent"))
 
-			fakeApigeeToken, _ := uuid.GenerateUUID()
-			if err := json.NewEncoder(w).Encode(map[string]string{
+			response := apigeeResponses[0]
+			apigeeResponses = apigeeResponses[1:]
+			if response.Body != nil {
+				_ = json.NewEncoder(w).Encode(response.Body)
+			}
+			w.WriteHeader(response.StatusCode)
+		}
+	}))
+}
+
+func TestRefreshToken_NotExpired(t *testing.T) {
+	apigeeCallCount := 0
+
+	fakeApigeeToken, _ := uuid.GenerateUUID()
+	issuedAt := time.Now().UnixMilli()
+	expiresIn := int64(1800)
+
+	testServer := setupTestServer(t, HttpResponses{
+		{
+			StatusCode: 200,
+			Body: map[string]interface{}{
 				"access_token": fakeApigeeToken,
 				"issued_at":    fmt.Sprintf("%d", issuedAt),
 				"expires_in":   fmt.Sprintf("%d", expiresIn),
-			}); err == nil {
-				w.WriteHeader(200)
-			}
-		}
-	}))
+			},
+		},
+	}, &apigeeCallCount)
 	defer testServer.Close()
 
 	client := NewBareMetalClient(testServer.URL, "test_user", "test_password", "test_account")
@@ -98,12 +119,14 @@ func TestRefreshToken_ExpiredToken(t *testing.T) {
 
 func TestRefreshToken_RetryableClient(t *testing.T) {
 	apigeeCallCount := 0
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if req.RequestURI == "/oauth/token" {
-			apigeeCallCount++
-			w.WriteHeader(500)
+	responses := make(HttpResponses, retryCount)
+	for i := 0; i < retryCount; i++ {
+		responses[i] = HttpResponse{
+			StatusCode: 500,
 		}
-	}))
+	}
+	testServer := setupTestServer(t, responses, &apigeeCallCount)
+	defer testServer.Close()
 
 	retryWaitTime = 1 * time.Second
 	retryMaxWaitTime = 1 * time.Second
