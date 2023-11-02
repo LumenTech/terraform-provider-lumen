8f795116c7ad866b02ebc358c454d6a257006de0
molsbee <molsbee@gmail.com>
2023-11-02 10:54:00 -0500

    Starting to work on bare metal server resource.

---
+++ lumen/resource_bare_metal_server.go
@@ -0,0 +1,41 @@
+package lumen
+
+import (
+	"context"
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"terraform-provider-lumen/lumen/validation"
+)
+
+func ResourceBareMetalServer() *schema.Resource {
+	return &schema.Resource{
+		Description: "Provision lumen bare metal server",
+		CreateContext: func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+			return nil
+		},
+		ReadContext: func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+			return nil
+		},
+		UpdateContext: func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+			return nil
+		},
+		DeleteContext: func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+			return nil
+		},
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateDiagFunc: func(i interface{}, path cty.Path) diag.Diagnostics {
+					validationError := validation.ValidateBareMetalServerName(i.(string))
+					if validationError != nil {
+						return diag.FromErr(validationError)
+					}
+					return nil
+				},
+			},
+		},
+	}
+}
