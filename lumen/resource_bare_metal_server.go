3537fa4b5e4c9487828f39253a7d929cafdcf4b5
molsbee <molsbee@gmail.com>
2023-11-02 11:41:52 -0500

    Working on schema definition for bare metal server resource.

--- lumen/resource_bare_metal_server.go
+++ lumen/resource_bare_metal_server.go
@@ -27,7 +27,6 @@ func ResourceBareMetalServer() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
-				ForceNew: true,
 				ValidateDiagFunc: func(i interface{}, path cty.Path) diag.Diagnostics {
 					if validationError := validation.ValidateBareMetalServerName(i.(string)); validationError != nil {
 						return diag.FromErr(validationError)
@@ -35,6 +34,167 @@ func ResourceBareMetalServer() *schema.Resource {
 					return nil
 				},
 			},
+			"location_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"configuration_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"os_image": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"network_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ConflictsWith: []string{
+					"network_name",
+					"network_size_id",
+				},
+			},
+			"network_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ConflictsWith: []string{
+					"network_id",
+				},
+			},
+			"network_size_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ConflictsWith: []string{
+					"network_id",
+				},
+			},
+			"username": {
+				Type:      schema.TypeString,
+				Required:  true,
+				Sensitive: true,
+			},
+			"password": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+				AtLeastOneOf: []string{
+					"password",
+					"public_key",
+				},
+			},
+			"public_key": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+				AtLeastOneOf: []string{
+					"password",
+					"public_key",
+				},
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"machine_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"machine_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"location": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"configuration": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"cores": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"memory": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"storage": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"disks": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"nics": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"processors": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"networks": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status_message": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ip": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vlan": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"status":         {},
+			"status_message": {},
+			"disks":          {},
+			"boot_disk":      {},
+			"service_id":     {},
+			"prices":         {},
+			"account_id":     {},
+			"created":        {},
+			"updated":        {},
 		},
 	}
 }
