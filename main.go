be3e5fde0f6fee20d7a6dbea6547de1048735fb7
molsbee <molsbee@gmail.com>
2023-11-03 11:18:23 -0500

    Added flags to logger to prevent double adding date timestamp to log outputs. Added to the client and enhanced testing to check for a scenario where get server by id should return no error and no server.

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
 
 	"terraform-provider-lumen/lumen"
 )
 
 func main() {
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return lumen.Provider()
