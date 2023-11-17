70db3d0e62e6e56c639e74a7ff010360ec1bca78
molsbee <molsbee@gmail.com>
2023-11-16 18:18:39 -0600

    Updated deleteContext and polling to simplify the logic along with lowered the timeout and used the delete timeout variable with the polling conf.

--- lumen/resource_bare_metal_server/delete.go
+++ lumen/resource_bare_metal_server/delete.go
@@ -2,17 +2,15 @@ package resource_bare_metal_server
 
 import (
 	"context"
-	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"strings"
 	"terraform-provider-lumen/lumen/client"
-	"terraform-provider-lumen/lumen/client/model/bare_metal"
 	"time"
 )
 
-var deleteTimeout = schema.DefaultTimeout(90 * time.Minute)
+var deleteTimeout = schema.DefaultTimeout(30 * time.Minute)
 
 func deleteContext(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
 	bmClient := i.(*client.Clients).BareMetal
@@ -20,21 +18,14 @@ func deleteContext(ctx context.Context, data *schema.ResourceData, i interface{}
 	server, err := bmClient.DeleteServer(serverId)
 	if err != nil {
 		return diag.FromErr(err)
-	} else if server == nil || strings.ToLower(server.Status) == "released" {
-		data.SetId("")
-		return nil
 	}
 
-	refreshResult, waitError := waitForServerDeletion(ctx, bmClient, serverId)
-	if waitError != nil {
-		return diag.FromErr(waitError)
+	if server != nil && strings.ToLower(server.Status) != "released" {
+		_, waitError := waitForServerDeletion(ctx, bmClient, serverId)
+		if waitError != nil {
+			return diag.FromErr(waitError)
+		}
 	}
-
-	fmt.Printf(
-		"[DEBUG] Deleted server (%s) final status (%s)",
-		serverId,
-		refreshResult.(bare_metal.Server).Status,
-	)
 	data.SetId("")
 	return nil
 }
@@ -48,27 +39,26 @@ func waitForServerDeletion(ctx context.Context, bmClient *client.BareMetalClient
 		Pending: deleteServerPendingStatus,
 		Target:  deleteServerTargetStatus,
 		Refresh: func() (interface{}, string, error) {
-			s, e := bmClient.GetServer(serverId)
-			if e != nil {
-				return nil, "", e
-			} else if s == nil {
-				return nil, "released", nil
+			server, err := bmClient.GetServer(serverId)
+			if err != nil {
+				return nil, "", err
 			}
 
-			found := false
-			for _, status := range deleteServerStatuses {
-				if status == strings.ToLower(s.Status) {
-					found = true
-					break
+			status := "unknown"
+			if server == nil {
+				status = "released"
+			} else {
+				for _, deleteStatus := range deleteServerStatuses {
+					if deleteStatus == strings.ToLower(server.Status) {
+						status = server.Status
+						break
+					}
 				}
 			}
 
-			if !found {
-				s.Status = "unknown"
-			}
-			return *s, strings.ToLower(s.Status), nil
+			return server, strings.ToLower(status), nil
 		},
-		Timeout:      90 * time.Minute,
+		Timeout:      *deleteTimeout,
 		Delay:        2 * time.Minute,
 		PollInterval: 30 * time.Second,
 	}
