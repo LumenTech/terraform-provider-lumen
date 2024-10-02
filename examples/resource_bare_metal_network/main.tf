9f75cfcb9f3b2b37d64e8cd3b4aa53d2e78f5da5
Alif Sarker <Alif.Sarker@lumen.com>
2024-10-02 11:36:11 -0700

    EDGEPP-17946 undo test changes to file

--- examples/resource_bare_metal_network/main.tf
+++ examples/resource_bare_metal_network/main.tf
@@ -15,39 +15,39 @@ provider "lumen" {
 
 resource "lumen_bare_metal_network" "network" {
   #  Example request data
-  name = "testNetwork24"
+  name = "testNetwork5"
   location_id = "DNVTCO56LEC"
   network_size_id = "6529723924b8bf31ebd998e2"
   network_type = "DUAL_STACK_INTERNET"
 }
 
-#resource "lumen_bare_metal_network" "network2" {
-  #  Create a private network with an existing VRF
-#  name = "testNetwork6"
-#  location_id = "DNVTCO56LEC"
-#  network_type = "PRIVATE"
-#  vrf = "88/VP12/003434/ASRT"
-#}
+resource "lumen_bare_metal_network" "network2" {
+   Create a private network with an existing VRF
+ name = "testNetwork6"
+ location_id = "DNVTCO56LEC"
+ network_type = "PRIVATE"
+ vrf = "88/VP12/003434/ASRT"
+}
 
-#resource "lumen_bare_metal_network" "network3" {
-  #  Create a private network with a new VRF
-#  name = "testNetwork7"
-#  location_id = "DNVTCO56LEC"
-#  network_type = "PRIVATE"
-#  vrf_description = "testPrivateNetwork"
-#}
+resource "lumen_bare_metal_network" "network3" {
+   Create a private network with a new VRF
+ name = "testNetwork7"
+ location_id = "DNVTCO56LEC"
+ network_type = "PRIVATE"
+ vrf_description = "testPrivateNetwork"
+}
 
 output "network" {
   value = lumen_bare_metal_network.network
   description = "Lumen bare metal network details"
 }
 
-#output "network2" {
-#  value = lumen_bare_metal_network.network2
-#  description = "Private network with existing VRF details"
-#}
+output "network2" {
+ value = lumen_bare_metal_network.network2
+ description = "Private network with existing VRF details"
+}
 
-#output "network3" {
-#  value = lumen_bare_metal_network.network3
-#  description = "Private network with new VRF details"
-#}
+output "network3" {
+ value = lumen_bare_metal_network.network3
+ description = "Private network with new VRF details"
+}
